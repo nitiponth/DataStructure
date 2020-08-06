a4d607859a2afce06d840e8cedfc0ee742298914
nitiponth <67319636+nitiponth@users.noreply.github.com>
2020-08-06 20:56:05 +0700

    Lab1 Finish

--- Lab1/src/Utils.java
+++ Lab1/src/Utils.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Random;
 
 public class Utils {
@@ -14,5 +15,40 @@ public class Utils {
             while(d[newIndex] != -1);
             d[newIndex] = temp[i];
         }
+    }
+
+    public void remove(int[] d,int i){
+        for(int j=i;j<d.length-1;j++)
+            d[j] = d[j+1];
+        d[d.length-1] = 0;
+    }
+
+    public void insert(int[] d,int i,int x){
+        for(int j=d.length-1;j>i;j--)
+            d[j] = d[j-1];
+        d[i] = x;
+    }
+
+    public boolean isLowerTriangular(double[][] d){
+        for(int i=0;i<d.length;i++){
+            for(int j=i+1;j<d[0].length;j++){
+                if(d[i][j] != 0)
+                    return false;
+            }
+        }
+        return true;
+    }
+
+    public int[] maxmin(int[] d){
+        int max=d[0], min=d[0];
+        for(int i=1;i<d.length;i++){
+            if(d[i] > max)
+                max = d[i];
+            if(d[i] < min)
+                min = d[i];
+        }
+        int[] ret = {max,min};
+
+        return ret;
     }
 }
